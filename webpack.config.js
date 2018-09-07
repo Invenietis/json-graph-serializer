9e64b39524fd424d936403c652d25eb25832d893
Benjamin Crosnier <benjamin.crosnier@signature-code.com>
2018-09-07 16:23:35 +0200

    Make a proper package out of serialize and deserialize

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'json-graph-serializer.js',
+        library: 'jsonGraphSerializer',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
